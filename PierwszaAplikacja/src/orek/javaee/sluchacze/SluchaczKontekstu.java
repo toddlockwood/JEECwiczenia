9853df38e8375e7595b20c7dbe8bbcf5246ab108
toddlockwood <arkadiusz.lapajski@hotmail.com>
2017-06-08 17:03:07 +0200

    init
    init

---
+++ PierwszaAplikacja/src/orek/javaee/sluchacze/SluchaczKontekstu.java
@@ -0,0 +1,20 @@
+package orek.javaee.sluchacze;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class SluchaczKontekstu implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
